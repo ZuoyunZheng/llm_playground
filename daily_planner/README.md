2f567a073c2843e7641bc3895ff077010a712816
Zuoyun Zheng <zuoyunzheng@gmx.de>
2025-01-27 18:23:44 +0100

    added ruff and readme

--- daily_planner/README.md
+++ daily_planner/README.md
@@ -1,11 +1,14 @@
 # Daily Planner
 
-Multi-agent system
+Multi-agent system with llama.cpp (C++/Python) backend
 
 ## Installation
 
 1. Install uv to manage packages
-   
+
 2. Make venv: `uv venv`
 
-3. Install deps: `CUDACXX="/usr/local/cuda/bin/nvcc" CMAKE_ARGS="-DGGML_CUDA=on" uv sync --no-cache -v`
+3. Install deps
+  a. If you have llama.cpp installed you don't need llama-cpp-python. You would need to start up your llama-server and pass the http port to a `LlamaCppServerProvider` instance
+  b. If you don't have llama.cpp installed, you would want to compile llama-cpp-python with CUDA support by passing the CMAKE flags: `CUDACXX="/usr/local/cuda/bin/nvcc" CMAKE_ARGS="-DGGML_CUDA=on" uv sync --no-cache -v`.<br />
+     Naturally you would be using a LlamaCppPythonProvider
--- daily_planner/main.py
+++ daily_planner/main.py
@@ -1,30 +1,47 @@
+import argparse
+
 from llama_cpp import Llama
 from llama_cpp_agent import LlamaCppAgent, MessagesFormatterType
 from llama_cpp_agent.providers import LlamaCppPythonProvider, LlamaCppServerProvider
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-MODEL_PATH = os.environ.get("MODEL_PATH")
+def main(args):
+    if args.backend == "python":
+        load_dotenv()
+        MODEL_PATH = os.environ.get("MODEL_PATH")
+        if args.model:
+            MODEL_PATH = args.model
 
-llama_model = Llama(MODEL_PATH, n_batch=1024, n_threads=4, n_gpu_layers=2)
-provider = LlamaCppPythonProvider(llama_model)
+        llama_model = Llama(MODEL_PATH, n_batch=1024, n_threads=4, n_gpu_layers=2)
+        provider = LlamaCppPythonProvider(llama_model)
+    else:
+        provider = LlamaCppServerProvider("http://127.0.0.1:8080")
 
-#provider = LlamaCppServerProvider("http://127.0.0.1:8080")
-agent = LlamaCppAgent(
-    provider,
-    system_prompt="You are a helpful assistant.",
-    predefined_messages_formatter_type=MessagesFormatterType.CHATML,
-)
+    agent = LlamaCppAgent(
+        provider,
+        system_prompt="You are a helpful assistant.",
+        predefined_messages_formatter_type=MessagesFormatterType.CHATML,
+    )
 
-settings = provider.get_provider_default_settings()
-settings.temperature = 0.65
+    settings = provider.get_provider_default_settings()
+    settings.temperature = 0.65
+
+    while True:
+        user_input = input(">")
+        if user_input == "exit":
+            break
+        agent_output = agent.get_chat_response(
+            user_input, llm_sampling_settings=settings
+        )
+        print(f"Agent: {agent_output.strip()}")
 
 
-while True:
-    user_input = input(">")
-    if user_input == "exit":
-        break
-    agent_output = agent.get_chat_response(user_input, llm_sampling_settings=settings)
-    print(f"Agent: {agent_output.strip()}")
+if __name__ == "__main__":
+    argparse = argparse.ArgumentParser("Daily Planner Agent")
+    argparse.add_argument("--backend", default="cpp", choices=["cpp", "python"])
+    argparse.add_argument("--model", help="Path to llama model", required=False)
+
+    args = argparse.parse_args()
+    main(args)
